3c7c6fb63bc65940af4a7b70992dd0e527c1bb9c
Otabek <otabekabdulazizov282@gmail.com>
2021-11-15 15:04:42 +0500

    zerojs

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict"
+let incr = 10,
+    decr = 10;
+console.log(incr++);
+console.log(--decr);
+let a = 25;
+if (a > 10 && a == 15) {
+    console.log("sucess");
+} else {
+    console.log("wrong")
+}
